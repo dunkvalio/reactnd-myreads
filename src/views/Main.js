6dde6f0e8458025aa4048d374bb9f97acf368bd3
Valentin Golchev <v.golchev@gmail.com>
2017-09-10 19:59:32 +0200

    refactor: Changed 'Main' to a stateless functional component
    Refactored the Main component to be a stateless functional component since it no longer need to manage state or lifecycle events.

--- src/views/Main.js
+++ src/views/Main.js
@@ -3,29 +3,26 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Shelf from '../components/Shelf'
 
-class Main extends React.Component {
-  render() {
-    const { books, onUpdate } = this.props;
-    const currentlyReading = books.filter(book => book.shelf === 'currentlyReading');
-    const wantToRead = books.filter(book => book.shelf === 'wantToRead');
-    const read = books.filter(book => book.shelf === 'read');
+const Main = ({ books, onUpdate }) => {
+  const currentlyReading = books.filter(book => book.shelf === 'currentlyReading');
+  const wantToRead = books.filter(book => book.shelf === 'wantToRead');
+  const read = books.filter(book => book.shelf === 'read');
 
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <Shelf title="Currently Reading" data={currentlyReading} onShelfUpdate={onUpdate} />
-          <Shelf title="Want to Read" data={wantToRead} onShelfUpdate={onUpdate} />
-          <Shelf title="Read" data={read} onShelfUpdate={onUpdate} />
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
-        </div>
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    );
-  }
+      <div className="list-books-content">
+        <Shelf title="Currently Reading" data={currentlyReading} onShelfUpdate={onUpdate} />
+        <Shelf title="Want to Read" data={wantToRead} onShelfUpdate={onUpdate} />
+        <Shelf title="Read" data={read} onShelfUpdate={onUpdate} />
+      </div>
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  );
 }
 
 Main.propTypes = {
