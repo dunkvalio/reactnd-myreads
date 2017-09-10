11461fa4c2a158f88c9654371958ebf4ae4204fe
Valentin Golchev <v.golchev@gmail.com>
2017-09-10 13:19:44 +0200

    feat: Implemented Search functionality on '/search' screen
    Implemented functionality to search for books based on input from the user.
    Added 'lodash' library to package.json + implemented 'throttle' to limit api invocation to 1 within a 2000ms (=2 seconds) timeframe.

--- src/views/Search.js
+++ src/views/Search.js
@@ -1,27 +1,45 @@
 import React from 'react';
+import _ from 'lodash';
+import * as api from '../utils/BooksAPI';
+import Book from '../components/Book';
 
 class Search extends React.Component {
+  state = {
+    books: [],
+  }
+
+  searchBooks = () => {
+    const query = this.refs.searchInput.value;
+    if(query) {
+      api.search(query)
+        .then(books => this.setState({ books }))
+        .catch(e => this.setState({ books: [] }));
+    }
+  }
+
   render() {
-    const { history } = this.props;
+    const { books } = this.state;
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={history.goBack}>Close</a>
+          <a className="close-search" onClick={this.props.history.goBack}>Close</a>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-
+            <input
+              type="text"
+              ref="searchInput"
+              placeholder="Search by title or author"
+              onChange={_.throttle(this.searchBooks, 2000)}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {books && books.map(book => (
+              <li key={book.id}>
+                <Book book={book} />
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
     );
